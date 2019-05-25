9cbbf1453cbdba7ebcc212a6d5ba70acd02b018d
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-05-25 17:20:06 +1000

    Basic page layout

---
+++ frontend/src/components/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import { palette } from 'styled-tools'
+
+export const CentralColumn = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+`
+function headingSize({ h1, h2, h3, h4, h5 }) {
+    if (h1) {
+      return 0
+    } else if (h2) {
+      return 1
+    } else if (h3) {
+      return 2
+    } else if (h4) {
+      return 3
+    } else if (h5) {
+      return 4
+    } else {
+      return 0
+    }
+  }
+
+export const Heading = styled.h1`
+  font-size: ${props => palette("headings", headingSize(props))};
+  line-height: ${props => palette("headings", headingSize(props))};
+  margin-bottom: 0;
+`
