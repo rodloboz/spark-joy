a6811fe913812ce3b4759a2d7c3e57e71f7dd6d3
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-03-24 17:19:16 +0000

    graphql mutiations for saving widget

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -1,20 +1,63 @@
-import { ApolloServer, gql } from 'apollo-server-lambda';
+// import { ApolloServer, gql } from 'apollo-server-lambda';
+import { ApolloServer, gql } from 'apollo-server';
+import AWS from 'aws-sdk';
+import uuidv4  from 'uuid/v4';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 const typeDefs = gql`
-  type Query{
-      hello: String
+  type Query {
+    hello: String
+  }
+
+  type Widget {
+    widgetId: String!
+    name: String!
+    thumbsUp: Int
+    thumbsDown: Int
+  }
+
+  type Mutation {
+    saveWidget(name: String!): Widget
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Hello world!"
+  },
+
+  Mutation: {
+    saveWidget: async (_: any, { name }: { name: string}) => {
+      const widgetId = uuidv4();
+
+      const result = await new Promise((resolve, reject) => {
+        dynamoDB.update({
+          TableName: process.env.DYNAMODB_TABLE!,
+          Key: { widgetId },
+          UpdateExpression: "SET widgetId = :widgetId, name = :name",
+          ExpressionAttributeValues: {
+            ":widgetId": widgetId,
+            ":name": name
+          }
+        });
+      });
+
+      console.log(result);
+
+      return {
+        name,
+        WidgetId: 'querty',
+        thumbsUp: 0,
+        thumbsDown: 0
+      };
+    }
   }
 };
-
-const server = new ApolloServer({
+ 
+export const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-export const handler = server.createHandler();
+// export const handler = server.createHandler();
---
+++ backend/src/localServer.ts
@@ -0,0 +1,7 @@
+import { server } from './graphql';
+
+console.log(server)
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
